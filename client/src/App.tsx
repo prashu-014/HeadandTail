c88431dfb0123841de12b5c48fea6b490cbfe01e
prashant <prashantburde84@gmail.com>
2025-07-09 17:31:51 +0530

    feat(add): Adding react router to easily naviage different page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,24 @@
-import './App.css'
-import Home from './pages/Home'
+import "./App.css";
+import Navigation from "./component/Navigation";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import HeadandTail from "./pages/HeadandTail";
+import Home from "./pages/Home";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-   <>
-   <Home />
-   
-   </>
-  )
+    <>
+    <Navigation />
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<About />} path="/about" />
+        <Route element={<HeadandTail />} path="/headandtail" />
+        <Route element={<Error />} path="*" />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
